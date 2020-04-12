ccb9512b8abb34ca47e0fa7beba6f5418123560e
jimihunter2002 <jimi.hunter008@gmail.com>
2020-04-12 04:27:24 +0100

    serve static content from backend asset

---
+++ build/precache-manifest.67be609f3518b4c5c18d20cadfa126d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc153105f07bab3a126af4f2ce86372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113ffa085def68f3eed3",
+    "url": "/static/css/main.497d2e59.chunk.css"
+  },
+  {
+    "revision": "d28c9fe466243757df0a",
+    "url": "/static/js/2.a30393cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a30393cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "113ffa085def68f3eed3",
+    "url": "/static/js/main.f8231744.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
---
+++ build/precache-manifest.ba66d0b134f2634f24bb8b269577ecf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfe1462fa00e9dd2623d58ad01f96e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2baa3e3f495809af877b",
+    "url": "/static/css/main.497d2e59.chunk.css"
+  },
+  {
+    "revision": "d28c9fe466243757df0a",
+    "url": "/static/js/2.a30393cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a30393cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2baa3e3f495809af877b",
+    "url": "/static/js/main.f8568b32.chunk.js"
+  },
+  {
+    "revision": "73cc5c71f716e545cadb",
+    "url": "/static/js/runtime-main.a70bf60b.js"
+  }
+]);
